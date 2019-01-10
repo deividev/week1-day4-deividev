59e2a92971a8f9d67c569a30e591edb1e431e5db
Marco Giacomuzzi <marco.giacomuzzi@gmail.com>
2019-01-10 16:10:24 +0100

    Update JS

--- toggler.js
+++ toggler.js
@@ -1,25 +1,30 @@
+function init() {
 
-// Dropdown
-// --------------------------------------------------------
-var dropdown = document.querySelector('.dropdown__btn');
+  // Dropdown
+  // --------------------------------------------------------
+  var dropdown = document.querySelector('.dropdown__btn');
 
-dropdown.onclick = function(event) {
-  var dropdown = this;
-  if (dropdown.hasAttribute('aria-expanded') && dropdown.getAttribute('aria-expanded') === 'false') {
-    dropdown.setAttribute('aria-expanded', 'true');
-  } else {
-    dropdown.setAttribute('aria-expanded', 'false');
+  dropdown.onclick = function(event) {
+    var dropdown = this;
+    if (dropdown.hasAttribute('aria-expanded') && dropdown.getAttribute('aria-expanded') === 'false') {
+      dropdown.setAttribute('aria-expanded', 'true');
+    } else {
+      dropdown.setAttribute('aria-expanded', 'false');
+    }
   }
-}
 
-// Search form
-// --------------------------------------------------------
-var input = document.querySelector('.search__input');
+  // Search form
+  // --------------------------------------------------------
+  var input = document.querySelector('.search__input');
 
-input.onfocus = function(e) {
-  this.setAttribute('aria-expanded', 'true');
-}
+  input.onfocus = function(e) {
+    this.setAttribute('aria-expanded', 'true');
+  }
 
-input.onblur = function(e) {
-  this.setAttribute('aria-expanded', 'false');
-}
+  input.onblur = function(e) {
+    this.setAttribute('aria-expanded', 'false');
+  }
+
+};
+
+window.onload = init;
